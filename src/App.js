2aa96e2cc8ffde6fe0204f34e4bf9002856ff0e1
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-10-29 00:11:00 -0300

    Roteamento com react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
+
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <p>Ola mundo</p>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
