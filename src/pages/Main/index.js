b0c9a0a48b4136389779892d5e52e402db59e27e
Victor Vilela Duarte <victorvduarte@gmail.com>
2019-11-05 01:37:11 -0200

    Navegação em rotas, propTypes e carregandos dados da API

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
-import { Container, Form, SubmitButton, List } from './styles';
+import { Link } from 'react-router-dom';
+import { Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
 
 import api from '../../services/api';
 
@@ -22,7 +24,7 @@ export default class Main extends Component {
 
   componentDidUpdate(_, prevState) {
     const { repositories } = this.state;
-    if (prevState.repositories !== this.state.repositories) {
+    if (prevState.repositories !== repositories) {
       localStorage.setItem('repositories', JSON.stringify(repositories));
     }
   }
@@ -78,7 +80,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
